803ca0e7b246f55a9b50e99fed0c8824e2ede75d
fahadaltaie <fahad_altaie@yahoo.com>
2020-06-04 15:33:55 +0200

    good

---
+++ test/php/view/section-logout.php
@@ -0,0 +1,9 @@
+
+        <section>
+            <script>
+            // ON VA SIMPLEMENT EFFACER LA CLE API DANS SESSION STORAGE
+            sessionStorage.removeItem('cleApi');
+            // REDIRECTION VERS LA PAGE D'ACCUEIL
+            location.href = 'index.php';
+            </script>
+        </section>
